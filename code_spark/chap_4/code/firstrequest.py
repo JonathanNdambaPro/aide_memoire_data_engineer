cfdac98e78d1da028643e110c0c9f0e1cf75b653
JonathanNdambaPro <jonathan.ndamba.pro@gmail.com>
2021-11-10 11:02:36 +0100

    Init

---
+++ code_spark/chap_4/code/firstrequest.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+
+p = Path(".").resolve()
+path_parent_folder = p.parent
+path_folder_data = path_parent_folder / "data"
+
+if __name__ == "__main__":
+    if path_folder_data.exists():
+        from pyspark.sql import SparkSession
+
+        spark = SparkSession.builder.appName("SparkSQLExampleApp").getOrCreate()
+
+        csv_file = path_folder_data / "departuredelays.csv"
+
+        df = (
+            spark.read.format("csv")
+            .option("inferSchema", "true")
+            .option("header", "true")
+            .load(f"{csv_file}")
+        )
+        df.createOrReplaceTempView("us_delay_flights_tbl")
+
+        query_1 = """SELECT distance, origin, destination 
+                     FROM us_delay_flights_tbl WHERE distance > 1000 
+                     ORDER BY distance DESC"""
+
+        query_2 = """SELECT date, delay, origin, destination 
+                      FROM us_delay_flights_tbl 
+                      WHERE delay > 120 AND ORIGIN = 'SFO' AND DESTINATION = 'ORD' 
+                      ORDER by delay DESC"""
+
+        query_3 = """SELECT delay, origin, destination,
+                CASE
+                  WHEN delay > 360 THEN 'Very Long Delays'
+                  WHEN delay > 120 AND delay < 360 THEN 'Long Delays'
+                  WHEN delay > 60 AND delay < 120 THEN 'Short Delays'
+                  WHEN delay > 0 and delay < 60  THEN  'Tolerable Delays'
+                  WHEN delay = 0 THEN 'No Delays'
+                  ELSE 'Early'
+               END AS Flight_Delays
+               FROM us_delay_flights_tbl
+               ORDER BY origin, delay DESC"""
+
+        all_query = [query_1, query_2, query_3]
+
+        for query in all_query:
+            spark.sql(query).show(10)
+        
+        df = spark.sql(query)
+        df.show(14)
+    else:
+        raise Exception("Le chemin indiue n'existe pas.")
---
+++ code_spark/chap_5/usr_defined_function.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+p = Path(".").resolve()
+path_parent_folder = p.parents[0]
+path_folder_data = path_parent_folder / "chap_4" / "data"
+
+if __name__ == "__main__":
+    if path_folder_data.exists():
+        from pyspark.sql import SparkSession
+        from pyspark.sql.types import LongType
+
+        spark = SparkSession.builder.appName("SparkSQLExampleApp").getOrCreate()
+
+        csv_file = path_folder_data / "departuredelays.csv"
+
+        df = (
+            spark.read.format("csv")
+            .option("inferSchema", "true")
+            .option("header", "true")
+            .load(f"{csv_file}")
+        )
+        df.createOrReplaceTempView("us_delay_flights_tbl")
+
+        # Create cubed function
+        def cubed(s): return s * s * s
+        # Register UDF
+        spark.udf.register("cubed", cubed, LongType())
+
+
+        query_0 = """SELECT distance, origin, destination 
+                     FROM us_delay_flights_tbl WHERE distance > 1000 
+                     ORDER BY distance DESC"""
+
+        query_1 = """SELECT cubed(distance), origin, destination 
+                     FROM us_delay_flights_tbl WHERE distance > 1000 
+                     ORDER BY distance DESC"""
+
+        query_2 = """SELECT cubed(distance), origin, destination 
+                     FROM us_delay_flights_tbl WHERE distance > 1000 
+                     ORDER BY distance DESC""" 
+
+        all_query = [query_0, query_1]
+
+        for query in all_query: 
+            spark.sql(query).show(10)
+
+        
+
+    else:
+        raise Exception("Le chemin n'existe pas.")
